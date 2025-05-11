2ba0e58d99bee410f0b3fcac275b4849818c11d9
TomiTJ <tomitjnguyen@gmail.com>
2025-05-11 14:35:59 +1000

    added HomeVIewModel

--- SVB-App/SVB-App/ViewModels/HomeViewModel.swift
+++ SVB-App/SVB-App/ViewModels/HomeViewModel.swift
@@ -5,4 +5,34 @@
 //  Created by Tomi Nguyen on 9/5/2025.
 //
 
-struct
+import Foundation
+import Combine
+
+class HomeViewModel: ObservableObject {
+    @Published var stocks: [Stock] = []
+    @Published var isLoading: Bool = false
+    @Published var error:String? = nil
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(){
+        fetchInitialStocks()
+    }
+    
+    func fetchInitialStocks(){
+        isLoading = true
+        error = nil
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            guard let self = self else { return }
+            //USE API HERE PLEASE IM JUST USING SAMPLE DATA FROM STOCK.SWIFT
+            self.stocks = Stock.sampleStocks
+            self.isLoading = false
+            
+        }
+    }
+    
+    func refreshStocks(){
+        
+    }
+}
