61e7d92a1dea76b30f1ba1eea4112097b97adef5
TomiTJ <tomitjnguyen@gmail.com>
2025-05-11 15:29:26 +1000

    Removed duplicate SVB app. Redid variables and implemented views for homeview. and added StrockRowVIew

--- SVB-App/SVB-App/Views/HomeView.swift
+++ SVB-App/SVB-App/Views/HomeView.swift
@@ -5,27 +5,33 @@
 //  Created by Tomi Nguyen on 9/5/2025.
 //
 
-/*import SwiftUI
+import SwiftUI
 
 struct HomeView: View {
-    //FavouriteViewModel
-    
-    @StateObject private var viewModel:homeViewModel = HomeViewModel();
-    @StateObject private var favouriteViewModel:FavouriteViewModel = FavouriteViewModel();
-    
+
+    @StateObject private var homeViewModel = HomeViewModel()
+    @StateObject private var favouritesViewModel = FavouritesViewModel()
     @State private var selectedTab : Tab = .allStocks
     
-
-}
-
-var body : some View {
-    NavigationView {
-        VStack {
-            //tab
-            Picker("Select View", selection: $selectedTab) {
+    enum Tab {
+        case allStocks
+        case favourites
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            VStack {
+                Picker("Select View", selection: $selectedTab) {
+                    Text("All Stocks").tag(Tab.allStocks)
+                    Text("Favourites").tag(Tab.favourites)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal)
+                .padding(.top)
                 
             }
         }
     }
+    
 }
-*/
