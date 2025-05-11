2b2f6dd911e3e873c7cc4053f8e56984df2f00cb
TomiTJ <tomitjnguyen@gmail.com>
2025-05-11 12:20:20 +1000

    Stock Constructor

--- SVB-App/SVB-App/Models/Stock.swift
+++ SVB-App/SVB-App/Models/Stock.swift
@@ -1,3 +1,40 @@
 //
 //  Stock.swift
 //
+import Foundation
+
+struct Stock : Identifiable, Codable, Hashable {
+    let id: UUID
+    let symbol: String
+    let name:String
+    var price: Double
+    var change: Double
+    var changePercentage : Double
+    var description:String?
+    
+    
+    init(id: UUID = UUID(), symbol: String,name:String, price: Double, change: Double,changePercentage: Double, description:String? = nil) {
+        self.id = id
+        self.symbol = symbol
+        self.name = name
+        self.price = price
+        self.change = change
+        self.changePercentage = changePercentage
+        self.description = description
+        
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(symbol)
+    }
+    
+    static func == (Ihs:Stock , rhs:Stock) -> Bool {
+        return Ihs.id == rhs.id && Ihs.symbol == rhs.symbol
+    }
+    
+    static var sampleStocks :[Stock] = [
+        Stock(symbol:"AAPL",name: "Apple Inc.", price: 170.34,change: 1.5,changePercentage: 0.89,description: "Technology")
+        Stock(symbol: "MSFT", name: "Microsoft Corporation", price: 280.5, change: -0.25, changePercentage: "Technology")
+    ]
+    }
+}
---
+++ SVB-App/SVB-App/Services/PersistenceService.swift
@@ -0,0 +1,57 @@
+//
+//  PersistenceService.swift
+//  SVB-App
+//
+//  Created by Tomi Nguyen on 11/5/2025.
+//
+
+import Foundation
+
+enum PersistenceError:Error {
+    case encodingFailed
+    case decodingFailed
+    case dataNotFound
+    
+}
+
+class PersistenceService {
+    static let shared = PersistenceService()
+    
+    private let userDefaults : UserDefaults
+    private let favoriteskey = "favouriteStocks"
+    
+    init(userDefaults: UserDefaults = .standard){
+        self.userDefaults = userDefaults
+    }
+    
+    func saveFavorites(_ favorites: [Stock]) throws {
+        
+        do {
+            let encoder = JSONEncoder()
+            let encodedData = try encoder.encode(stocks)
+            userDefaults.set(encodedData, forKey: favoriteskey)
+            
+        }catch {
+            print("FAILED TO ENCODE FAVOURITES STOCKS: \(error)")
+            throw PersistenceService.encodingFailed
+        }
+    }
+ func loadFavorites() throws -> [Stock] {
+        guard let savedData  = userDefaults.data(forKey: favoriteskey) else {
+            return []
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([Stock].self, from: savedData)
+        } catch {
+            return[]
+        }
+    }
+    
+    func clearRFavorites() {
+        userDefaults.removeObject(forKey: favoriteskey)
+    }
+    
+}
+
