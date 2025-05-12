d3dd43c96953515bc653fe5d738dc2afb6bf7449
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2025-05-12 21:48:32 +1000

    linked stockdetaiview to alert view

--- SVB-App/SVB-App/SVB_AppApp.swift
+++ SVB-App/SVB-App/SVB_AppApp.swift
@@ -2,6 +2,7 @@
 //  SVB_AppApp.swift
 //
 import SwiftUI
+import SwiftData
 
 // will need to change later, just set to this as this is our only viewß
 @main
@@ -10,10 +11,21 @@ struct SVB_AppApp: App {
         stockService: StockService()
     )
 
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([Alert.self])
+        let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [config])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             SearchView()
                 .environmentObject(searchVM)
+                .modelContainer(sharedModelContainer)
         }
     }
 }
